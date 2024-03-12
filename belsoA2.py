8df122079059e4f8fa65bfaf353ad7e10f8caa4a
Isti-k <kiscserkes03@gmail.com>
2024-03-12 10:53:59 +0100

    robot

---
+++ belsoA2.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env pybricks-micropython
+from pybricks.hubs import EV3Brick
+from pybricks.ev3devices import (Motor, TouchSensor, ColorSensor, InfraredSensor, UltrasonicSensor, GyroSensor)
+from pybricks.parameters import Port, Stop, Direction, Button, Color
+from pybricks.tools import wait, StopWatch, DataLog
+from pybricks.robotics import DriveBase
+from pybricks.media.ev3dev import SoundFile, ImageFile
+
+class Palya():
+
+    def __init__(self):
+        # tégla
+        self.ev3 = EV3Brick()
+        # motorok
+        self.jm = Motor(Port.B)
+        self.bm = Motor(Port.C)
+        self.km = Motor(Port.A)
+        # szenzorok
+        self.cs = ColorSensor(Port.S3)
+        self.ts = TouchSensor(Port.S1)
+        self.gs = GyroSensor(Port.S2)
+        self.us = UltrasonicSensor(Port.S4)
+        #self.ir = InfraredSensor(Port.S4)
+
+        # dupla motorkezelő
+        self.robot = DriveBase(self.jm, self.bm, 55, 90)
+
+    def palya2(self):
+        self.robot.settings(straight_speed=800)
+        self.robot.straight(1500)
+        self.robot.turn(-125)
+        self.robot.straight(1100)
+        self.robot.turn(-125)
+        self.robot.straight(1600)
+        self.robot.turn(-125)
+        self.robot.straight(1200)
+        self.robot.turn(-130)
+        self.robot.straight(500)
+        self.robot.turn(-720)
+        self.robot.turn(720)
+        self.robot.stop(Stop.BRAKE)
+
+    def koszon(self):
+        self.ev3.speaker.set_volume(100)
+        self.ev3.speaker.play_file("teve.wav")
---
+++ vonalkovetoA2.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env pybricks-micropython
+from pybricks.hubs import EV3Brick
+from pybricks.ev3devices import (Motor, TouchSensor, ColorSensor, InfraredSensor, UltrasonicSensor, GyroSensor)
+from pybricks.parameters import Port, Stop, Direction, Button, Color
+from pybricks.tools import wait, StopWatch, DataLog
+from pybricks.robotics import DriveBase
+from pybricks.media.ev3dev import SoundFile, ImageFile
+
+class Palya():
+
+    def __init__(self):
+        # tégla
+        self.ev3 = EV3Brick()
+        # motorok
+        self.jm = Motor(Port.B)
+        self.bm = Motor(Port.C)
+        self.km = Motor(Port.A)
+        # szenzorok
+        self.cs = ColorSensor(Port.S3)
+        self.ts = TouchSensor(Port.S1)
+        self.gs = GyroSensor(Port.S2)
+        self.us = UltrasonicSensor(Port.S4)
+        #self.ir = InfraredSensor(Port.S4)
+
+        # dupla motorkezelő
+        self.robot = DriveBase(self.jm, self.bm, 55, 90)
+
+    def palya2(self):
+        self.robot.settings(straight_speed=800)
+        while True:
+            if cs.reflection() > 28:
+                bm.run(200)
+                jm.run(100)
+            else:
+                jm.run(200)
+                bm.run(100)
+        
+    def koszon(self):
+        self.ev3.speaker.set_volume(100)
+        self.ev3.speaker.play_file("teve.wav")
+
